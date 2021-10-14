88ce0f4b917bb7b88f0bd20c106e62fb0bc7cae5
yakushIvan <yakush.grand@gmail.com>
2021-10-14 11:49:33 +0300

    add calculate net endpoint

--- app.js
+++ app.js
@@ -11,6 +11,16 @@ const calculateGross = (net, tax, extraTax) => {
     }
 }
 
+const calculateNet = (gross, tax, extraTax) => {
+    const net = Math.ceil((1 - (Number(tax) / 100)) * Number(gross) - Number(extraTax))
+    return {
+        gross,
+        tax: `${tax}%`,
+        extraTax,
+        net
+    }
+}
+
 app.get('/', (req, res) => {
     res.send('This is Pure Endpoint')
 })
@@ -20,5 +30,12 @@ app.get('/gross/:net/:tax/:extraTax', (req, res) => {
     const gross = calculateGross(net, tax, extraTax)
     res.send(gross)
 })
+
+app.get('/net/:gross/:tax/:extraTax', (req, res) => {
+    const { gross, tax, extraTax } = req.params
+    const net = calculateNet(gross, tax, extraTax)
+    res.send(net)
+})
+
 
 app.listen(3000, () => console.log('Server has been started on port 3000...'))
